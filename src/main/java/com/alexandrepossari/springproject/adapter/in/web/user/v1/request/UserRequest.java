d79210b025e4e4de23d35c30524ff657f23b21b6
Alexandre Possari <ac.possari@gmail.com>
2022-01-23 13:14:47 -0300

    Adicionar classe de request e responde do usuário

---
+++ src/main/java/com/alexandrepossari/springproject/adapter/in/web/user/v1/request/UserRequest.java
@@ -0,0 +1,31 @@
+package com.alexandrepossari.springproject.adapter.in.web.user.v1.request;
+
+public class UserRequest {
+    private String email;
+    private String password;
+    private String nome;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
